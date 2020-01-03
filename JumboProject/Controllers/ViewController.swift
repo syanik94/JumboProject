3be59760a3bcff1972470ef5e673d2e76b1bf70a
Yanik <syanik@yahoo.com>
2020-01-03 08:26:37 -0600

    Initial commit

---
+++ JumboProject/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  JumboProject
+//
+//  Created by Yanik Simpson on 1/3/20.
+//  Copyright © 2020 Yanik Simpson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
---
+++ JumboProject/Supporting Files/SceneDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  SceneDelegate.swift
+//  JumboProject
+//
+//  Created by Yanik Simpson on 1/3/20.
+//  Copyright © 2020 Yanik Simpson. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+
+        // Use a UIHostingController as window root view controller.
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UINavigationController(rootViewController: ViewController())
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+
+
+}
+
